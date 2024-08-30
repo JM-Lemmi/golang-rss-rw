607d640b6edbbd6a788ba03900714c1c2cd5b050
Julian Lemmerich <jle@cirosec.de>
2024-08-30 10:53:51 +0200

    feat: create ParseRSS and WriteRSS functions

--- main.go
+++ main.go
@@ -2,50 +2,37 @@ package rss
 
 import (
 	"encoding/xml"
-	"net/http"
-	"time"
+	"io"
 )
 
-func main() {
-	http.HandleFunc("/", rssHandler)
-	http.ListenAndServe(":3000", nil)
+type Item struct {
+	Title       string `xml:"title"`
+	Link        string `xml:"link"`
+	Description string `xml:"description"`
+	PubDate     string `xml:"pubDate"`
 }
 
-func rssHandler(w http.ResponseWriter, r *http.Request) {
-	type Item struct {
-		Title       string `xml:"title"`
-		Link        string `xml:"link"`
-		Description string `xml:"description"`
-		PubDate     string `xml:"pubDate"`
-	}
+type Rss struct {
+	XMLName     xml.Name `xml:"rss"`
+	Version     string   `xml:"version,attr"`
+	Title       string   `xml:"channel>title"`
+	Description string   `xml:"channel>description"`
+	Link        string   `xml:"channel>link"`
 
-	type rss struct {
-		Version     string `xml:"version,attr"`
-		Description string `xml:"channel>description"`
-		Link        string `xml:"channel>link"`
-		Title       string `xml:"channel>title"`
+	Item []Item `xml:"channel>item"`
+}
 
-		Item []Item `xml:"channel>item"`
-	}
-
-	articles := []Item{
-		{"foo", "http://mywebsite.com/foo", "lorem ipsum", time.Now().Format(time.RFC1123Z)},
-		{"foo2", "http://mywebsite.com/foo2", "lorem ipsum2", time.Now().Format(time.RFC1123Z)}}
-
-	feed := &rss{
-		Version:     "2.0",
-		Description: "My super website",
-		Link:        "http://mywebsite.com",
-		Title:       "Mywebsite",
-		Item:        articles,
-	}
-
-	x, err := xml.MarshalIndent(feed, "", "  ")
+func ParseRSS(r io.Reader) (Rss, error) {
+	var rss Rss
+	err := xml.NewDecoder(r).Decode(&rss)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
+		return Rss{}, err
 	}
+	return rss, nil
+}
 
-	w.Header().Set("Content-Type", "application/xml")
-	w.Write(x)
+func (rss Rss) WriteRSS(w io.Writer) error {
+	enc := xml.NewEncoder(w)
+	enc.Indent("", "  ")
+	return enc.Encode(rss)
 }
